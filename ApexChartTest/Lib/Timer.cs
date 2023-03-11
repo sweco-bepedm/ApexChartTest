90c1bfcc0ab2ecbf91bb2566691c4dae9ccbeaa8
Pedro <pedro@LENOVO>
2023-03-11 12:27:21 +0100

    Add project files.

---
+++ ApexChartTest/Lib/Timer.cs
@@ -0,0 +1,31 @@
+﻿namespace ApexChartTest.Lib
+{
+    public class Timer
+    {
+        readonly DateTime t0;
+        readonly string unit;
+        DateTime tN;
+        public Timer(string unit = "seconds")
+        {
+            t0 = tN = DateTime.Now;
+            this.unit = unit;
+        }
+        public void Elapsed(string name)
+        {
+            if (unit == "seconds")
+                Console.WriteLine(name + " executed in " + (DateTime.Now - tN).TotalSeconds.ToString("0.0 s"));
+            else
+                Console.WriteLine(name + " executed in " + (DateTime.Now - tN).TotalMilliseconds.ToString("0 ms"));
+
+            tN = DateTime.Now;
+        }
+        public string End()
+        {
+            string msg = "Total execution time: " + (DateTime.Now - t0).TotalSeconds.ToString("0.0 s");
+
+            Console.WriteLine(msg);
+
+            return msg;
+        }
+    }
+}
---
+++ ApexChartTest/Lib/ValuePoint.cs
@@ -0,0 +1,32 @@
+﻿namespace ApexChartTest.Lib
+{
+    public class ValuePoint
+    {
+        public string Position { get; set; }
+        public decimal Value { get; set; }
+
+        public ValuePoint() { }
+        public ValuePoint(string position, double value)
+        {
+            this.Position = position;
+            this.Value = Convert.ToDecimal(value);
+        }
+        public static List<ValuePoint> ReturnEmpty()
+        {
+            List<ValuePoint> list = new();
+
+            for (int i = 0; i < 7 * 24; i++)
+            {
+                var point = new ValuePoint()
+                {
+                    Position = i.ToString(),
+                    Value = 0
+                };
+
+                list.Add(point);
+            }
+
+            return list;
+        }
+    }
+}
